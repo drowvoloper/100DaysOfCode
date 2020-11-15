a0df6bf0b04d96a30c142aa8abdbc852fe2797c2
drowvoloper <drowvoloper.contact@gmail.com>
2020-11-15 04:03:56 +0100

    day 7

--- README.txt
+++ README.txt
@@ -2,5 +2,6 @@ Day 1: Reviewing Node. I even wrote an article in Spanish about the difference b
 Day 2: Reviewing events and read line in Node. 
 Day 3: I learnt about the readline module, read about the documentation about it and played a little to understand everything better.
 Day 4: Yep.. Node review! I started a little D&D Character Creator
-Day 5: D&D Character Creator
-Day 6: D&D Character Creator
+Day 5: Node js!
+Day 6: Node js!
+Day 7: Still Node js!
--- dndCharacterCreator/app.js
+++ dndCharacterCreator/app.js
@@ -1,67 +1,27 @@
+const EventEmitter = require('events');
+const eventEmitter = new EventEmitter();
+
 const readline = require('readline');
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-// variables
-//const steps = ['race', 'class', 'alignment', 'scores', 'skills', 'equipment', 'name'];
-const stepsList = ['race', 'class'];
-let indexStep = 0;
-let currentStep = stepsList[indexStep];
-const steps = {
-    race: [
-        {
-            question: 'What is your race?\n',
-            answers: ['human', 'elf']
-        }
-    ],
-    class: [
-        {
-            question: 'What is your class?\n',
-            answers: ['rogue', 'ranger']
-        },
-        {
-            question: 'What is your background?\n',
-            answers: ['black', 'white']
-        }
-    ]
-};
-let indexQuestion = 0;
-let currentQuestion = steps[currentStep][indexQuestion].question;
-let possibleAnswers = steps[currentStep][indexQuestion].answers;
-
+// questions
+const questions = [
+    {
+        question: 'What is your class?\n',
+        answers: ['barbarian', 'fighter', 'ranger', 'rogue', 'cleric', 'druid', 'monk', 'paladin', 'bard', 'sorcerer', 'warlock', 'wizard'],
+        chosen: null
+    },
+    {
+        question: 'What is your race?\n',
+        answers: ['human', 'elf', 'dwarf', 'halfling', 'half-elf', 'half-orc', 'gnome', 'dragonborn', 'tiefling'],
+        chosen: null
+    }
+]
 
 // intro 
 const intro = `Welcome, adventurer. My name is Mur and I'm here to help you build an awesome, unique character! Are you ready?
 a. Yess. Let's do this!
 b. No, sorry. Maybe later.\n`;
-
-rl.question(intro, (answer) => {
-    if (answer.trim().toLowerCase() === 'a') {
-        //console.log('yayy');
-        console.log(currentQuestion);
-        rl.on('line', (answer) => {
-            console.log(possibleAnswers);
-
-            if (indexQuestion < steps[currentStep].length) {
-                ++indexQuestion;
-                currentQuestion = steps[currentStep][indexQuestion].question;
-                possibleAnswers = steps[currentStep][indexQuestion].answers;
-            } else {
-                ++indexStep;
-                currentStep = stepsList[indexStep];
-                indexQuestion = 0;
-                currentQuestion = steps[currentStep][indexQuestion].question;
-                possibleAnswers = steps[currentStep][indexQuestion].answers;
-            }
-
-            console.log(currentQuestion);
-        });
-    } else {
-        rl.close();
-    }
-});
-
-// app closed
-rl.on('close', () => console.log('bye, bye :('));
