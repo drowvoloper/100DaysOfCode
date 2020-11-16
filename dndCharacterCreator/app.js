fbe9e41a8a9d24032b7dfb9f25a05b8cd640c125
drowvoloper <drowvoloper.contact@gmail.com>
2020-11-16 15:09:38 +0100

    dnd wip..

--- dndCharacterCreator/app.js
+++ dndCharacterCreator/app.js
@@ -1,27 +1,42 @@
-const EventEmitter = require('events');
-const eventEmitter = new EventEmitter();
-
-const readline = require('readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-// questions
-const questions = [
-    {
-        question: 'What is your class?\n',
-        answers: ['barbarian', 'fighter', 'ranger', 'rogue', 'cleric', 'druid', 'monk', 'paladin', 'bard', 'sorcerer', 'warlock', 'wizard'],
-        chosen: null
-    },
-    {
-        question: 'What is your race?\n',
-        answers: ['human', 'elf', 'dwarf', 'halfling', 'half-elf', 'half-orc', 'gnome', 'dragonborn', 'tiefling'],
-        chosen: null
+const Personaje = class {
+    constructor() { /* ... */};
+    set raza(value) { 
+        this.raza = value; 
     }
-]
 
-// intro 
-const intro = `Welcome, adventurer. My name is Mur and I'm here to help you build an awesome, unique character! Are you ready?
-a. Yess. Let's do this!
-b. No, sorry. Maybe later.\n`;
+    get raza() {
+        return this.raza;
+    }
+}
+
+const razas = ['dracónido', 'elfo', 'enano', 'gnomo', 'humano', 'mediano', 'tiefling', 'semielfo', 'semiorco'];
+const clases = ['pícaro', 'mago', 'paladín', 'guerrero', 'monje', 'hechicero', 'clérigo', 'druida', 'explorador', 'brujo', 'bardo', 'bárbaro']
+const caracteristicas = ['fuerza', 'destreza', 'constitución', 'inteligencia', 'sabiduría', 'carisma'];
+const habilidades = {
+    fue: ['atletismo'],
+    des: ['acrobacias', 'juego de manos', 'sigilo'],
+    int: ['arcano', 'historia', 'investigación', 'naturaleza', 'religión'],
+    sab: ['trato con animales', 'medicina', 'percepción', 'perspicacia', 'supervivencia'],
+    car: ['engaño', 'intimidación', 'interpretación', 'persuasión']
+}
+
+// Raza
+
+
+// Nombre
+
+// Clase
+
+// Puntos de característica
+
+// Habilidades
+
+// Dotes
+
+// Oro inicial
+
+// Equipo
+
+// Clase de armadura y bonos
+
+
--- node-review/fcc-nodejs-tutorial/app.js
+++ node-review/fcc-nodejs-tutorial/app.js
@@ -1,43 +1,7 @@
 const fs = require('fs');
-// create a file
-/*fs.writeFile(
-    'example.txt', 
-    'this is an example',
-    (err) => {
-        if(err)
-            console.log(err);
-        else {
-            console.log('File successfully created!');
-            fs.readFile('example.txt', 'utf8', (err, file) => {
-                if(err)
-                    console.log(err);
-                else {
-                    console.log(file);
-                }
-            });
-        }         
-})*/
-
-/*fs.rename('example.txt', 'example2.txt', (err) => {
+fs.mkdir('tutorial', (err) => {
     if(err)
         console.log(err);
     else
-        console.log('Successfully renamed the file');
-});*/
-
-/*fs.appendFile(
-    'example2.txt', 
-    'Some data added',
-    (err) => {
-        if(err)
-            console.log(err);
-        else
-            console.log('Successfully added data!')
-})*/
-
-fs.unlink('example2.txt', (err) => {
-    if(err)
-        console.log(err);
-    else
-        console.log('Bye, bye, file')
+        console.log('folder created!!');
 })
